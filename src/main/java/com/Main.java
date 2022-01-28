ad6c6db9282a2651d989ef5d5f3ea7b0fadcbfd4
Joshua Suchit <86627712+Josh-S0@users.noreply.github.com>
2022-01-28 13:53:50 +0000

    Spring initialization

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,13 +1,13 @@
 package com;
 
-import com.database.MongoConnector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MongoConnector.getInstance();
-		
+		SpringApplication.run(Main.class, args);
 	}
 
 }
